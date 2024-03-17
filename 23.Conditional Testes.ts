b180d5686293b47267ce3c831e22247136da5492
MUHAMMAD ASIF <162907407+Muhammadasif1986@users.noreply.github.com>
2024-03-17 20:06:53 +0500

    Add files via upload

---
+++ 23.Conditional Testes.ts
@@ -0,0 +1,56 @@
+// 23.	Conditional Tests: Write a series of conditional tests.
+//  Print a statement describing each test and your prediction for the results of each test. 
+//  Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+
+
+
+
+// (=) set/assign
+// (==) to check value
+//(===) to check value and types
+// (!=) to check Not Equal
+// 
+
+
+
+
+
+
+
+// 01
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru');
+// 02
+console.log("Is car === 'subaru'? I predict True.")
+console.log(car === 'subaru');
+// 03
+console.log("Is car != 'honda'? I predict True.")
+console.log(car != 'honda');
+// 04
+console.log("Is car !== 'honda'? I predict True.")
+console.log(car !== 'honda');
+// 05
+console.log("Is car.Upper case == 'SUBARU'? I predict True.")
+console.log(car.toUpperCase() == 'SUBARU');
+// 06
+console.log("Is car == 'SUBARU'? I predict false.")
+console.log(car == 'SUBARU');
+// 07
+console.log("Is car === 'SUBARU'? I predict false.")
+console.log(car === 'SUBARU');
+// 08
+console.log("Is car == 'Train'? I predict false.")
+console.log(car == 'Train');
+// 09
+console.log("Is car != 'subaru'? I predict false.")
+console.log(car != 'subaru');
+// /10
+console.log("Is car === 'Bus'? I predict false.")
+console.log(car === 'Bus');
