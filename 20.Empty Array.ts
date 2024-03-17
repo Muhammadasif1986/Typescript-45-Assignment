d765490549334a556e4567c920b167f1fb6f958e
MUHAMMAD ASIF <162907407+Muhammadasif1986@users.noreply.github.com>
2024-03-17 20:03:20 +0500

    Add files via upload

---
+++ 20.Empty Array.ts
@@ -0,0 +1,10 @@
+// 20.	Think of something you could store in a array. For example,
+//  you could make a list of mountains, rivers, countries, cities, languages,
+  
+let item=['Pakistan','Sindh','urdu'];
+// or anything else you’d like. Write a program that creates a list containing these items.
+let item2 :string[]=[];
+item2.push('saudia Arabia');
+item2.push('Iraq');
+item2.push('Iran');
+console.log(item2)
